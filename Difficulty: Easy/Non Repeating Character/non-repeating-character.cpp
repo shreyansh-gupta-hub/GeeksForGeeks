cad9b89aea544b940a887acf442320a5810794ca
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2025-06-17 18:06:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            bool found = false;
+    
+            for (int j = 0; j < n; ++j) {
+                if (i != j && s[i] == s[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) 
+                return s[i];
+        }
+    
+        return '$';
+    }
+};
