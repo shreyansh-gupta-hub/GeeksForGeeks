f9b219482affaf686eb27cf0b90ce6fbfd85b82d
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-12 13:15:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -1,23 +1,33 @@
 // User function template for C++
 class Solution {
   public:
+    int lowerbound(vector<int> &arr, int n1){
+        int low = 0;
+        int high = n1-1;
+        int ans = n1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            //mabye an answer
+            if(arr[mid]>=1){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
     int rowWithMax1s(vector<vector<int>> &arr) {
         // code here
         int n1 = arr[0].size();
         int n2 = arr.size();
         int maxidx{-1};
-        int max = INT_MIN;
+        int max = 0;
         
         for (int i{0};i<n2;i++){
-            int sum{0};
             if(arr[i][0] == 1) return i;
             if (arr[i][n1-1] == 0) continue;
-            for(int j{0}; j<n1-1;j++){
-                if(arr[i][j]==1){
-                    sum++;
-                }
-                
-            }
+            int sum = n1 - lowerbound(arr[i],n1);
             if(sum>max){
                     max = sum;
                     maxidx = i;
