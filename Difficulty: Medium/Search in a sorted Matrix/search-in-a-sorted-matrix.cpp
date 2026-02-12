36435eb264cd60a23d8102d740869d0d314d5a78
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-12 15:42:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    
+    bool searchMatrix(vector<vector<int>> &arr, int target) {
+        // code here
+        int n = arr.size();
+        int m = arr[0].size();
+        if(arr.size()==0) return false;
+        int low{0};
+        int high = (n*m)-1;
+        while(low<= high){
+            int mid = (low+high)/2;
+            int i = mid/m;
+            int j = mid%m;
+            if(arr[i][j]==target) return true;
+            else if(arr[i][j]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
+};
