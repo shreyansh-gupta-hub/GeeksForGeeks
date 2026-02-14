2aa8b081ee5eec499f1f739eb48562150e03a971
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-14 13:25:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Medium/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,19 +1,48 @@
 class Solution {
   public:
+    int upperBound(vector<int> &arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;  
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;      
+                high = mid - 1;   
+            } else {
+                low = mid + 1;   
+            }
+        }
+        return ans;  
+    }
+    int countsmallequal(vector<vector<int>> &mat, int n, int m, int x){
+        int count{0};
+        for (int i{0};i<n;i++){
+            count += upperBound(mat[i], x, m);
+        }
+        return count;
+    }
     int median(vector<vector<int>> &mat) {
         // code here
         int n  = mat.size();
         int m = mat[0].size();
-        vector<int> arr;
-        for (auto i : mat){
-            for(auto j: i){
-                arr.push_back(j);
+        int low = INT_MAX;
+        int high = INT_MIN;
+        for (int i{0}; i<n;i++){
+            if(mat[i][0]<low) low = mat[i][0];
+            if(mat[i][m-1]>high) high = mat[i][m-1];
+        }
+        int req = (n*m)/2;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int smallequal = countsmallequal(mat,n,m,mid);
+            if(smallequal>req){
+                high = mid-1;
+            }else{
+                low = mid +1;
             }
         }
-        sort(arr.begin(),arr.end());
-        int len = arr.size();
-        len /=2;
-        return arr[len];
-        
+        return low;
     }
 };
