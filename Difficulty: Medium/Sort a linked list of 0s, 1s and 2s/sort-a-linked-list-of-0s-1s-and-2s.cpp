f3fc303549307be3ff958da6e750a74092cb2ac4
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-15 17:16:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -12,33 +12,45 @@
 */
 class Solution {
   public:
+
     Node* sortList(Node *head){
-    // Write your code here.
-    int count1{0};
-    int count2{0};
-    int count0{0};
+    // Write your code here.\
     if (head ==NULL || head->next == NULL) return head;
+
+    Node* oneDummy = new Node(-1);
+    Node* twoDummy = new Node(-1);
+    Node* zeroDummy = new Node(-1);
+
+    Node* zeroTail = zeroDummy;
+    Node* oneTail = oneDummy;
+    Node* twoTail = twoDummy;
+
+
     Node* temp = head;
     while(temp!=NULL){
         int x = temp->data;
-        if(x==0) count0++;
-        else if (x==1) count1++;
-        else count2++;
+        if(x==0){
+            zeroTail->next = temp;
+            zeroTail = zeroTail->next;
+        } 
+        else if (x==1){
+            oneTail->next = temp;
+            oneTail = oneTail->next;            
+        }
+        else{
+            twoTail->next = temp;
+            twoTail = twoTail->next;            
+        }
         temp = temp->next;
     }
-    temp = head;
-    for (int i{0}; i<count0;i++){
-        if(temp!=NULL){temp->data = 0;
-        temp = temp->next;}
-    }
-    for (int i{0}; i<count1;i++){
-            if(temp!=NULL){temp->data = 1;
-            temp = temp->next;}
-        }
-    for (int i{0}; i<count2;i++){
-            if (temp!=NULL){temp->data = 2;
-            temp = temp->next;}
-        }
+    
+    zeroTail->next = oneDummy->next ? oneDummy->next : twoDummy->next;
+    oneTail->next = twoDummy->next;
+    twoTail->next = nullptr;
+    head = zeroDummy->next;
+    delete zeroDummy;
+    delete oneDummy;
+    delete twoDummy;
     return head;
 
     }
