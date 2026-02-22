7aeef319f64aae95180ff669e5634764f72dc817
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-22 18:43:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    double power(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+
+    double fastPow(double x, long long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) return half * half;
+        else return half * half * x;
+    }
+};
