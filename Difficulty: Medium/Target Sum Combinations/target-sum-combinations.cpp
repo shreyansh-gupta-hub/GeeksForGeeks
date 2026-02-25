e02d3169aa0d7488ea1a17a0905c0f3efc25d6e9
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-25 14:28:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Target Sum Combinations/target-sum-combinations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    void solve(int ind, int k, vector<int>& arr, vector<int>& res, vector<vector<int>>& ans){
+        
+        if(ind==arr.size()){
+            if(k==0){
+                ans.push_back(res);
+            }
+            return;
+        }
+
+        //pick
+        if (arr[ind]<=k){
+            res.push_back(arr[ind]);
+            solve(ind,k-arr[ind],arr,res,ans );
+            res.pop_back();
+        }
+        //not pick
+        solve(ind+1,k,arr,res,ans );
+    }
+    vector<vector<int>> targetSumComb(vector<int> &arr, int target) {
+        // code here
+        vector<vector<int>> ans;
+        vector<int> res;
+        solve(0,target,arr,res,ans);
+        return ans;
+    }
+};
